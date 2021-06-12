a42ed0ad5a46ee0c188d9ac64fa7513048218219
Kenneth Aasan <k.aasan@sportradar.com>
2021-06-12 17:55:28 +0200

    init

--- packages/frontend/src/pages/Users/Users.tsx
+++ packages/frontend/src/pages/Users/Users.tsx
@@ -1,24 +1,28 @@
-import { Grid } from '@material-ui/core';
-import { DataGrid, GridColumns } from '@material-ui/data-grid';
+import { Box, Grid } from '@material-ui/core';
+import {
+  DataGrid,
+  GridCellParams,
+  GridColumns,
+  GridRowData,
+} from '@material-ui/data-grid';
 import React from 'react';
 import { Container } from '../../components/Container';
 import { Loading } from '../../components/Loading';
 import { useGetUsersQuery } from '../../generated/queries';
 import { ErrorPage } from '../ErrorPage';
 
-const columns: GridColumns = [
-  {
-    field: 'name',
-    headerName: 'Navn',
-    flex: 1,
-  },
-  {
-    field: 'points',
-    headerName: 'Poeng',
-    flex: 1,
-    type: 'number',
-  },
-];
+function getWidth(question: string): number {
+  switch (question) {
+    case 'Hvem går videre til 8-delsfinaler?':
+      return 850;
+    case 'Kvartfinalelag':
+      return 450;
+    case 'Semifinalelag':
+      return 250;
+    default:
+      return 200;
+  }
+}
 
 export function Users() {
   const { error, loading, data } = useGetUsersQuery();
@@ -30,16 +34,65 @@ export function Users() {
     return <Loading />;
   }
 
-  const rows =
-    data && data.getUsers
-      ? data.getUsers.map((user) => {
-          return {
-            id: user.id,
-            name: user.name,
-            points: user.points,
-          };
-        })
-      : [];
+  const columns: GridColumns = [
+    {
+      field: 'name',
+      headerName: 'Navn',
+      width: 160,
+    },
+    {
+      field: 'points',
+      headerName: 'Poeng',
+      width: 108,
+      type: 'number',
+      renderCell: (params: GridCellParams) => {
+        return <strong>{params.value}</strong>;
+      },
+    },
+  ];
+
+  const rows: GridRowData[] = [];
+
+  if (data && data.getUsers) {
+    data.getUsers[0].questions?.forEach((question) => {
+      columns.push({
+        field: question.question,
+        headerName: question.question,
+        width: getWidth(question.question),
+        renderCell: (params: GridCellParams) => {
+          let color = undefined;
+
+          if (question.blueprint) {
+            if (question.blueprint === params.value) {
+              color = 'success.main';
+            }
+          } else {
+            color = 'text.disabled';
+          }
+
+          return (
+            <Box component="span" color={color}>
+              {params.value}
+            </Box>
+          );
+        },
+      });
+    });
+
+    data.getUsers.forEach((user) => {
+      const row: { [key: string]: string | number } = {
+        id: user.id,
+        name: user.name,
+        points: user.points,
+      };
+
+      user.questions?.forEach((question) => {
+        row[question.question] = question.answer;
+      });
+
+      rows.push(row);
+    });
+  }
 
   return (
     <Container>
