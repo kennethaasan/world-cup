40973ff295762fb963bb9b804408fe5114a5fcc1
Kenneth Aasan <k.aasan@sportradar.com>
2022-11-21 21:11:44 +0100

    fix: fixes aws creds

--- packages/cdk/src/cdk.ts
+++ packages/cdk/src/cdk.ts
@@ -2,6 +2,11 @@ import { App } from 'aws-cdk-lib';
 import { WorldCupStack } from './cdk-stack';
 import { getEnvVar } from './env';
 
+console.log('AWS_ACCOUNT_ID', getEnvVar('AWS_ACCOUNT_ID'));
+console.log('AWS_DEFAULT_REGION', getEnvVar('AWS_DEFAULT_REGION'));
+console.log('AWS_ACCESS_KEY_ID', getEnvVar('AWS_ACCESS_KEY_ID'));
+console.log('AWS_SECRET_ACCESS_KEY', getEnvVar('AWS_SECRET_ACCESS_KEY'));
+
 export function deploy() {
   const app = new App();
 
